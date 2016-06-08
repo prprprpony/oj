8fe92e0340d9d19cc7be77da6a854781c4f3d4e7
prpr <3254tg89@gmail.com>
2016-06-08 18:20:29 +0800

    uva 530: divide by gcd to prevent int overflow

--- uva/ac/530.cpp
+++ uva/ac/530.cpp
@@ -1,12 +1,24 @@
 #include <cstdio>
 using namespace std;
+int gcd(int a, int b)
+{
+    int t;
+    while (b) {
+        t = a % b;
+        a = b;
+        b = t;
+    }
+    return a;
+}
 int C(int n, int k)
 {
     if (k > n - k)
         k = n - k;
-    long long ret = 1;
-    for (int i  = 1; i <= k; ++i)
-        ret = ret * (n - k + i) / i;
+    int ret = 1;
+    for (int i  = 1; i <= k; ++i) {
+        int g = gcd(ret, i);
+        ret = ret / g * ((n - k + i) / (i / g));
+    }
     return ret;
 }
 int main()
