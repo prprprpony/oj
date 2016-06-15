7dcfccf97cb9eb53e6db258b331d4eb221e8b707
prpr <3254tg89@gmail.com>
2016-06-15 21:03:41 +0800

    zj b537: faster way

---
+++ zj/ac/b537/b537.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/* naive way
+long long find(int a, int b)
+{
+    if (a > b)
+        return 2 * find(a - b, b);
+    else if (a < b)
+        return 1 + find(b, a);
+    else
+        return 1;
+}
+*/
+long long find(int a, int b)
+{
+    if (a > b) 
+        return (a % b) ? (1 + find(b, a % b)) << (a / b) : 1LL << (a / b - 1);
+    else if (a < b)
+        return 1 + find(b, a);
+    else
+        return 1;
+}
+
+int main(void)
+{
+    int a, b;
+    while (scanf("%d%d", &a, &b) == 2) 
+        printf("%lld\n", find(a, b));
+    return 0;
+}
