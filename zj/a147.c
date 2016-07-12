c0717ce2f2828cfc794b344741185427b9c5342f
prpr <3254tg89@gmail.com>
2016-07-12 23:41:16 +0800

    zj a147: fix undefined behavior

--- zj/a147.c
+++ zj/a147.c
@@ -1,7 +1,7 @@
 int scanf(const char *, ...);
 int printf(const char *, ...);
 int putchar(int);
-int getint(int new) {static int x; return (new && scanf("%d", &x) == 1 && 0) | x;}
+int getint(int new) {static int x; return new && scanf("%d", &x) == 1, x;}
 int printall(int n) {return n && printall(n - 1) || (n % 7 && (n - 1 && !putchar(' ') || printf("%d", n) < 1));}
 int solve(void) {return getint(1) && (printall(getint(0) - 1) || !putchar('\n') || solve());}
 int main(void) {return solve();}
