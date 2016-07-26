863e708ac525670abc5725f5c997c414bdf9cd41
prpr <3254tg89@gmail.com>
2016-07-26 13:29:07 +0800

    World CodeSprint 5: Longest Increasing Subsequence Arrays: improve efficiency

--- hackerrank/World-CodeSprint-5/Longest-Increasing-Subsequence-Arrays.cpp
+++ hackerrank/World-CodeSprint-5/Longest-Increasing-Subsequence-Arrays.cpp
@@ -1,11 +1,9 @@
 #include <cstdio>
-#include <algorithm>
 using namespace std;
 typedef long long ll;
 const int M = 1e9 + 7;
-const int maxn = 5.4e5;
-int INV[maxn];
-int n_1p[maxn];
+const int maxn = 5e5;
+int INV[maxn + 1];
 void exgcd(int a, int b, ll & x, ll & y, ll & g)
 {
 	if (b) {
@@ -27,35 +25,33 @@ inline int add(int a, int b) {return (a += b) < M ? a : a - M;}
 inline int sub(int a, int b) {return (a -= b) < 0 ? a : a + M;}
 inline int mul(int a, int b) {return a * 1LL * b % M;}
 inline int _div(int a, int b) {return mul(a, INV[b]);}
-int C(int n, int k)
+int qpow(int a, int p)
 {
-	if (k > n)
-		return 0;
-	k = min(k, n - k);
 	int ret = 1;
-	for (int i = 1; i <= k; ++i)
-		ret = _div(mul(ret, n - k + i), i);
+	for ( ; p; p >>= 1, a = mul(a, a))
+		if (p & 1)
+			ret = mul(ret, a);
 	return ret;
 }
 int main()
 {
 	int p;
 	scanf("%d", &p);
-	for (int i = 1; i <= 5e5; ++i)
+	for (int i = 1; i <= maxn; ++i)
 		INV[i] = inv(i);
 	while (p--) {
 		int m, n;
 		scanf("%d%d", &m, &n);
 		int ans = 0;
-		n_1p[0] = 1;
-		for (int i = 1; i <= m - n; ++i)
-			n_1p[i] = mul(n_1p[i - 1], n - 1);
-		int k = C(m - 1, n - 1);
-		int np = 1;
-		for (int i = 0; i <= m - n; ++i) {
-			ans = add(ans, mul(mul(k, n_1p[m - i - n]), np));
-			k = _div(mul(k, m - i - n), m - i - 1);
-			np = mul(np, n);
+		int k = 1;
+		int n_1p = 1;
+		int np = qpow(n, m - n);
+		for (int i = n; i <= m; ++i) {
+			//k = C(i - 1, n - 1)
+			ans = add(ans, mul(mul(k, n_1p), np));
+			k = _div(mul(k, i), i - (n - 1));
+			n_1p = mul(n_1p, n - 1);
+			np = _div(np, n);
 		}
 		printf("%d\n", ans);
 	}
