2992d7c6b15e31aad5fc9d581345eedd97c2a40f
prpr <3254tg89@gmail.com>
2016-09-11 15:32:28 +0800

    add compile tools && default code

---
+++ tools/compile-with-precompiled-headers.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+g++ -I ~/github/oj/tools/ -include stdc++.h -g -O2 "$1"
---
+++ tools/precompile-headers.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+g++ -Winvalid-pch -x c++-header -g -O2 -o ./stdc++.h.gch /usr/include/c++/6.2.1/x86_64-pc-linux-gnu/bits/stdc++.h 
