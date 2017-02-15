7a67c31437cda5f68b5f1b18ff891b1fbb2b3566
prpr <3254tg89@gmail.com>
2017-02-15 09:08:31 +0800

    cf 765: Round #397 (Div. 1 + Div. 2): solved 4/7; ranked 837

---
+++ cf/765/D.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+// nichijou
+#define REP(i,s,e) for (int i = (s), __e = (e); i < __e; ++i)
+#define RP(i,n) REP(i,0,n)
+#define PER(i,s,e) for (int i = (s) - 1, __e = (e); i >= __e; --i)
+#define PR(i,n) PER(i,n,0)
+#define DO(n) REP(__i,0,n)
+template<typename T>
+void cmax(T & a, T b) {a = max(a, b);}
+template<typename T>
+void cmin(T & a, T b) {a = min(a, b);}
+
+// data type
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define F first
+#define S second
+
+// STL container
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define SZ(a) ((int)a.size())
+#define ALL(a) a.begin(), a.end()
+#define CLR(a) a.clear()
+#define BK(a) (a.back())
+#define FT(a) (a.front())
+#define DB(a) a.pop_back()
+#define DF(a) a.pop_front()
+#define PB push_back
+#define EB emplace_back
+
+/* reading input is now 20% cooler */
+bool RD(void) {return true;}
+bool RD(int & a) {return scanf("%d", &a) == 1;}
+bool RD(ll & a) {return scanf("%lld", &a) == 1;}
+bool RD(double & a) {return scanf("%lf", &a) == 1;}
+bool RD(char & a) {return scanf(" %c", &a) == 1;}
+bool RD(char * a) {return scanf("%s", a) == 1;}
+template<typename T, typename ... TT>
+bool RD(T & a, TT & ...  b) {return RD(a) && RD(b...);}
+
+/* For it's time for you to fulfill your output */
+void PT(const int a) {printf("%d", a);}
+void PT(const ll a) {printf("%lld", a);}
+void PT(const double a) {printf("%.16f", a);}
+void PT(const char a) {putchar(a);}
+void PT(const char * a) {fputs(a, stdout);}
+
+/* The line will last forever! */
+void PL(void) {PT('\n');}
+template<typename T, typename ... TT>
+void PL(const T a, const TT ...  b) {PT(a); if (sizeof...(b)) PT(' '); PL(b...);}
+
+/* Good Luck && Have Fun ! */
+const int N = 1.12e5;
+bool vis[N];
+int f[N],g[N],h[N];
+int main()
+{
+    int n;
+    RD(n);
+    REP(i,1,n+1) RD(f[i]);
+    int c = 0;
+    REP(i,1,n+1) if (!vis[i]) {
+        if (i == f[i]) {
+            ++c;
+            g[i] = c;
+            h[c] = i;
+            vis[i] = true;
+        } else if (f[i] == f[f[i]]) {
+            if (!vis[f[i]]) {
+                ++c;
+                g[f[i]] = c;
+                h[c] = f[i];
+                vis[f[i]] = true;
+            }
+            vis[i] = true;
+            g[i] = g[f[i]];
+        } else {
+            PL(-1);
+            return 0;
+        }
+    }
+    PL(c);
+    REP(i,1,n+1) PT(g[i]), PT(" \n"[i==n]);
+    REP(i,1,c+1) PT(h[i]), PT(" \n"[i==c]);
+}
