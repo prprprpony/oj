6bfa192c15102a2ce6fcc4da655b953d949b7527
prpr <3254tg89@gmail.com>
2017-03-14 20:27:30 +0800

    cf 782F: Thue–Morse sequence, doubling, binary search, bitset

---
+++ cf/782F.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+// nichijou
+#define REP(i,a,b) for (int i = (a), __e = (b); i < __e; ++i)
+#define RP(i,n) REP(i,0,n)
+#define PER(i,s,e) for (int i = (s) - 1, __e = (e); i >= __e; --i)
+#define PR(i,n) PER(i,n,0)
+#define REP1(i,a,b) for (int i = (a), __e = (b); i <= __e; ++i)
+#define RP1(i,n) REP1(i,1,n)
+#define PER1(i,s,e) for (int i = (s), __e = (e); i >= __e; --i)
+#define PR1(i,n) PER1(i,n,1)
+#define DO(n) REP(__i,0,n)
+template<typename T>
+void cmax(T & a, T b) {a = max(a, b);}
+template<typename T>
+void cmin(T & a, T b) {a = min(a, b);}
+
+// data type
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define F first
+#define S second
+
+// STL container
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define SZ(a) ((int)a.size())
+#define ALL(a) a.begin(), a.end()
+#define CLR(a) a.clear()
+#define BK(a) (a.back())
+#define FT(a) (a.front())
+#define DB(a) a.pop_back()
+#define DF(a) a.pop_front()
+#define PB push_back
+#define EB emplace_back
+
+/* Reading input is now 20% cooler! */
+bool RD(void) {return true;}
+bool RD(int & a) {return scanf("%d", &a) == 1;}
+bool RD(ll & a) {return scanf("%lld", &a) == 1;}
+bool RD(double & a) {return scanf("%lf", &a) == 1;}
+bool RD(char & a) {return scanf(" %c", &a) == 1;}
+bool RD(char * a) {return scanf("%s", a) == 1;}
+template<typename T, typename ... TT>
+bool RD(T & a, TT & ...  b) {return RD(a) && RD(b...);}
+
+/* Do princesses dream of magic sheep? */
+#define DRI(a) int a; RD(a)
+#define DRII(a,b) DRI(a); DRI(b)
+#define DRIII(a,b,c) DRI(a); DRII(b,c)
+#define DRIIII(a,b,c,d) DRI(a); DRIII(b,c,d)
+
+/* For it's time for you to fulfill your output. */
+void PT(const int a) {printf("%d", a);}
+void PT(const ll a) {printf("%lld", a);}
+void PT(const double a) {printf("%.16f", a);}
+void PT(const char a) {putchar(a);}
+void PT(const char * a) {fputs(a, stdout);}
+
+/* The line will last forever! */
+void PL(void) {PT('\n');}
+template<typename T, typename ... TT>
+void PL(const T a, const TT ...  b) {PT(a); if (sizeof...(b)) PT(' '); PL(b...);}
+
+/* Good Luck && Have Fun ! */
+#define RPS(i,b) for (int i = b._Find_first(); i < SZ(b); i = b._Find_next(i))
+const int N = 500;
+const ll inf = 1000000000000000000LL;
+typedef bitset<N> bs;
+bs g[60][N][2];
+int main()
+{
+	DRII(n,m);
+	DO(m) {
+		DRIII(u,v,t);
+		--u,--v;
+		g[0][u][t][v] = 1;
+	}
+	for (int h=0;h<59;++h)
+		RP(u,n) 
+			RP(t,2)
+				RPS(v,g[h][u][t])
+					g[h+1][u][t] |= g[h][v][t^1];
+	ll ans = 0;
+	int b = 1;
+	bs bt[2];
+	bt[1].set();
+	for (int h = 59; h>=0;--h) {
+		RPS(u,bt[b])
+			bt[b^1] |= g[h][u][b^1];
+		if (bt[b^1].any()) {
+			ans |= 1LL<<h;
+			bt[b].reset();
+			b^=1;
+		}
+	}
+	PL(ans >= inf ? -1 : ans);
+}
