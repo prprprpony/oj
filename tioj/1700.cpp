b96e25dd974a880910885e9e74f4805db1dad7a8
prpr <3254tg89@gmail.com>
2017-04-10 22:11:06 +0800

    tioj 1700: gcd

---
+++ tioj/1700.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+// read integers
+int RI() {return 0;}
+template<typename T>
+int RI(T & a)
+{
+	int c;
+	while (!((c = getchar()) == '-' || isdigit(c) || c == EOF));
+	if (c == EOF)
+		return 0;
+	a = 0;
+	if (c == '-') {
+		while (isdigit(c = getchar()))
+			a = 10 * a - (c & 15);
+	} else {
+		do {
+			a = 10 * a + (c & 15);
+		} while (isdigit(c = getchar()));
+	}
+	return 1;
+}
+template<typename T, typename... Args>
+int RI(T & a, Args & ... args) {return RI(a) ? 1 + RI(args...) : 0;}
+//print integers, python style
+template<typename T>
+void __PI(T a)
+{
+	static const int maxd = 25;
+	static char d[maxd];
+	int i = maxd - 1;
+	int s = a < 0 ? -1 : 1;
+	do {
+		d[--i] = s * (a % 10) + '0';
+	} while (a /= 10);
+	if (s < 0)
+		d[--i] = '-';
+	fputs(d + i, stdout);
+}
+template<char sep>
+void __PSI() {}
+template<char sep, typename T>
+void __PSI(const T & a) {putchar(sep), __PI(a);}
+template<char sep, typename T, typename... Args>
+void __PSI(const T & a, const Args & ... args) {__PSI<sep, T>(a), __PSI<sep, Args...>(args...);}
+template<char sep = ' ', char end = '\n', typename T, typename... Args>
+void PI(const T & a, const Args & ... args) {__PI(a), __PSI<sep, Args...>(args...), putchar(end);}
+
+int main()
+{
+	int n;
+	while (RI(n)) {
+		int ans = 0, x;
+		while (n--) {
+			RI(x);
+			ans = __gcd(ans,x);
+		}
+		PI(ans);
+	}
+}
