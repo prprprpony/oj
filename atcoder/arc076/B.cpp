0656b9c812db74e313106b04ee29320128b2d927
prpr <3254tg89@gmail.com>
2017-06-24 22:30:33 +0800

    AtCoder arc076: solved 2/4, ranked 200

---
+++ atcoder/arc076/B.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+using namespace std;
+// nichijou
+#define REP(i,a,b) for (int i(a), _B(b); i < _B; ++i)
+#define RP(i,n) REP(i,0,n)
+#define PER(i,a,b) for(int i((a)-1), _B(b); i >= _B; --i)
+#define PR(i,n) PER(i,n,0)
+#define REP1(i,a,b) REP(i,a,(b)+1)
+#define RP1(i,n) REP1(i,1,n)
+#define PER1(i,a,b) PER(i,(a)+1,b)
+#define PR1(i,n) PER1(i,n,1)
+#define DO(n) REP(__i,0,n)
+template<typename T>
+void cmax(T & a, T b) {a = max(a, b);}
+template<typename T>
+void cmin(T & a, T b) {a = min(a, b);}
+
+// data type
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define F first
+#define S second
+
+// STL container
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define SZ(a) ((int)a.size())
+#define ALL(a) a.begin(), a.end()
+#define CLR(a) a.clear()
+#define BK(a) (a.back())
+#define FT(a) (a.front())
+#define DB(a) a.pop_back()
+#define DF(a) a.pop_front()
+#define PB push_back
+#define EB emplace_back
+
+/* I gave you my heart and then you turned around. */
+void _BG(const char * s) {}
+template<typename T, typename ... TT>
+void _BG(const char * s,T a, TT...b)
+{
+	for (int c = 0; *s && (c || *s != ','); ++s) {
+		cerr<<*s;
+		switch (*s) {
+		case '(':
+		case '[':
+		case '{':
+			++c;
+			break;
+		case ')':
+		case ']':
+		case '}':
+			--c;
+			break;
+		}
+	}
+	cerr<<" = "<<a;
+	if (*s) {
+		cerr<<", ";
+		_BG(++s,b...);
+	} else
+		cerr<<endl;
+}
+#define BG(...) do { \
+	cerr << __PRETTY_FUNCTION__ << ':' << __LINE__ << ": "; \
+	_BG(#__VA_ARGS__,__VA_ARGS__); \
+} while(0)
+
+/* Reading input is now 20% cooler! */
+bool RD(void) {return true;}
+bool RD(char & a) {return scanf(" %c", &a) == 1;}
+bool RD(char * a) {return scanf("%s", a) == 1;}
+bool RD(double & a) {return scanf("%lf", &a) == 1;}
+bool RD(int & a) {return scanf("%d", &a) == 1;}
+bool RD(ll & a) {return scanf("%lld", &a) == 1;}
+
+template<typename T, typename ... TT>
+bool RD(T & a, TT & ...  b) {return RD(a) && RD(b...);}
+
+/* Do princesses dream of magic sheep? */
+#define RI(a) int a; RD(a)
+#define RII(a,b) RI(a); RI(b)
+#define RIII(a,b,c) RI(a); RII(b,c)
+#define RIIII(a,b,c,d) RI(a); RIII(b,c,d)
+
+/* For it's time for you to fulfill your output. */
+void PT(const char a) {putchar(a);}
+void PT(const char * a) {fputs(a, stdout);}
+void PT(char * a) {fputs(a, stdout);}
+void PT(const double a) {printf("%.16f", a);}
+void PT(const int a) {printf("%d", a);}
+void PT(const ll a) {printf("%lld", a);}
+
+/* The line will last forever! */
+template<char sep = ' ',char end = '\n'>
+void PL(void) {if (end) PT(end);}
+template<char sep = ' ',char end = '\n',typename T, typename ... TT>
+void PL(const T a, const TT ...  b) {PT(a); if (sizeof...(b) && sep) PT(sep); PL<sep,end>(b...);}
+
+/* Good Luck && Have Fun ! */
+const int N = 1e5 + 87;
+struct po
+{
+	int x,y,i;
+} p[N];
+vector< pair<int,pii> > v;
+int g[N];
+int find(int x) {return g[x] == x ? x : g[x] = find(g[x]);}
+int main()
+{
+	RI(n);
+	RP(i,n) {
+		RD(p[i].x,p[i].y);
+		p[i].i = i;
+	}
+	sort(p,p+n,[](po a, po b)->bool{return a.x < b.x;});
+	REP(i,1,n)
+		v.EB(p[i].x-p[i-1].x,pii{p[i].i,p[i-1].i});
+	sort(p,p+n,[](po a, po b)->bool{return a.y < b.y;});
+	REP(i,1,n)
+		v.EB(p[i].y-p[i-1].y,pii{p[i].i,p[i-1].i});
+	ll ans = 0;
+	sort(ALL(v));
+	RP(i,n) g[i] = i;
+	for (auto e : v) {
+		int u = find(e.S.F);
+		int v = find(e.S.S);
+		if (u == v)
+			continue;
+		g[u] = v;
+		ans += e.F;
+	}
+	PL(ans);
+}
