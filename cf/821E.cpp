e24cc4f2563c04bc7c2fd8d6c6b161ab08cde573
prpr <3254tg89@gmail.com>
2017-06-26 23:44:08 +0800

    cf 821E: dp, matrix fast exponentiation, O(NK^2 log(V/N) + K^4 log V), K = 16, N = 100, V = 10^18

---
+++ cf/821E.cpp
@@ -0,0 +1,152 @@
+#include <bits/stdc++.h>
+using namespace std;
+// nichijou
+#define REP(i,a,b) for (int i(a), _B(b); i < _B; ++i)
+#define RP(i,n) REP(i,0,n)
+#define PER(i,a,b) for(int i((a)-1), _B(b); i >= _B; --i)
+#define PR(i,n) PER(i,n,0)
+#define REP1(i,a,b) REP(i,a,(b)+1)
+#define RP1(i,n) REP1(i,1,n)
+#define PER1(i,a,b) PER(i,(a)+1,b)
+#define PR1(i,n) PER1(i,n,1)
+#define DO(n) REP(__i,0,n)
+template<typename T>
+void cmax(T & a, T b) {a = max(a, b);}
+template<typename T>
+void cmin(T & a, T b) {a = min(a, b);}
+
+// data type
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define F first
+#define S second
+
+// STL container
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define SZ(a) ((int)a.size())
+#define ALL(a) a.begin(), a.end()
+#define CLR(a) a.clear()
+#define BK(a) (a.back())
+#define FT(a) (a.front())
+#define DB(a) a.pop_back()
+#define DF(a) a.pop_front()
+#define PB push_back
+#define EB emplace_back
+
+/* I gave you my heart and then you turned around. */
+void _BG(const char * s) {}
+template<typename T, typename ... TT>
+void _BG(const char * s,T a, TT...b)
+{
+	for (int c = 0; *s && (c || *s != ','); ++s) {
+		cerr<<*s;
+		switch (*s) {
+		case '(':
+		case '[':
+		case '{':
+			++c;
+			break;
+		case ')':
+		case ']':
+		case '}':
+			--c;
+			break;
+		}
+	}
+	cerr<<" = "<<a;
+	if (*s) {
+		cerr<<", ";
+		_BG(++s,b...);
+	} else
+		cerr<<endl;
+}
+#define BG(...) do { \
+	cerr << __PRETTY_FUNCTION__ << ':' << __LINE__ << ": "; \
+	_BG(#__VA_ARGS__,__VA_ARGS__); \
+} while(0)
+
+/* Reading input is now 20% cooler! */
+bool RD(void) {return true;}
+bool RD(char & a) {return scanf(" %c", &a) == 1;}
+bool RD(char * a) {return scanf("%s", a) == 1;}
+bool RD(double & a) {return scanf("%lf", &a) == 1;}
+bool RD(int & a) {return scanf("%d", &a) == 1;}
+bool RD(ll & a) {return scanf("%lld", &a) == 1;}
+
+template<typename T, typename ... TT>
+bool RD(T & a, TT & ...  b) {return RD(a) && RD(b...);}
+
+/* Do princesses dream of magic sheep? */
+#define RI(a) int a; RD(a)
+#define RII(a,b) RI(a); RI(b)
+#define RIII(a,b,c) RI(a); RII(b,c)
+#define RIIII(a,b,c,d) RI(a); RIII(b,c,d)
+
+/* For it's time for you to fulfill your output. */
+void PT(const char a) {putchar(a);}
+void PT(const char * a) {fputs(a, stdout);}
+void PT(char * a) {fputs(a, stdout);}
+void PT(const double a) {printf("%.16f", a);}
+void PT(const int a) {printf("%d", a);}
+void PT(const ll a) {printf("%lld", a);}
+
+/* The line will last forever! */
+template<char sep = ' ',char end = '\n'>
+void PL(void) {if (end) PT(end);}
+template<char sep = ' ',char end = '\n',typename T, typename ... TT>
+void PL(const T a, const TT ...  b) {PT(a); if (sizeof...(b) && sep) PT(sep); PL<sep,end>(b...);}
+
+/* Good Luck && Have Fun ! */
+const int K = 16, M = 1e9+7, LG = 60;
+int add(int a,int b) {return (a += b) < M ? a : a - M;}
+int mul(int a,int b) {return a * 1LL * b % M;}
+struct mat
+{
+	int r,c,a[K][K];
+	mat() {}
+	mat(int _r,int _c) : r(_r), c(_c)
+	{
+		RP(i,r)
+			fill_n(a[i],c,0);
+	}
+	mat operator * (const mat & x)
+	{
+		mat y(r,x.c);
+		RP(i,r)
+			RP(j,x.c)
+				RP(k,c)
+					y.a[i][j] = add(y.a[i][j], mul(a[i][k], x.a[k][j]));
+		return y;
+	}
+} p[K][LG];
+int main()
+{
+	RP(c,K) {
+		p[c][0].r = p[c][0].c = K;
+		RP(i,c+1)
+			REP1(j,i-1,i+1)
+				if (0<=j&&j<=c)
+					p[c][0].a[i][j] = 1;
+		REP(i,1,LG)
+			p[c][i] = p[c][i-1] * p[c][i-1];
+	}
+	int n;
+	ll k;
+	RD(n,k);
+	mat ans(K,1);
+	ans.a[0][0] = 1;
+	DO(n) {
+		ll a,b;
+		int c;
+		RD(a,b,c);
+		cmin(b,k);
+		PER(i,K,c+1)
+			ans.a[i][0] = 0;
+		for (ll x = b - a, i = 0; x; x >>= 1, ++i)
+			if (x & 1)
+				ans = p[c][i] * ans;
+	}
+	PL(ans.a[0][0]);
+}
