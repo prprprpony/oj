d89912206c73e7d2308a2a1bff67780da41392d4
prpr <3254tg89@gmail.com>
2017-06-29 03:17:34 +0800

    cf 782: Round #403 (Div.2): solved 2/6, ranked 949

---
+++ cf/782/B.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+// nichijou
+#define REP(i,a,b) for (int i = (a), __e = (b); i < __e; ++i)
+#define RP(i,n) REP(i,0,n)
+#define PER(i,s,e) for (int i = (s) - 1, __e = (e); i >= __e; --i)
+#define PR(i,n) PER(i,n,0)
+#define REP1(i,a,b) for (int i = (a), __e = (b); i <= __e; ++i)
+#define RP1(i,n) REP1(i,1,n)
+#define PER1(i,s,e) for (int i = (s), __e = (e); i >= __e; --i)
+#define PR1(i,n) PER1(i,n,1)
+#define DO(n) REP(__i,0,n)
+template<typename T>
+void cmax(T & a, T b) {a = max(a, b);}
+template<typename T>
+void cmin(T & a, T b) {a = min(a, b);}
+
+// data type
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define F first
+#define S second
+
+// STL container
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define SZ(a) ((int)a.size())
+#define ALL(a) a.begin(), a.end()
+#define CLR(a) a.clear()
+#define BK(a) (a.back())
+#define FT(a) (a.front())
+#define DB(a) a.pop_back()
+#define DF(a) a.pop_front()
+#define PB push_back
+#define EB emplace_back
+
+/* Reading input is now 20% cooler! */
+bool RD(void) {return true;}
+bool RD(int & a) {return scanf("%d", &a) == 1;}
+bool RD(ll & a) {return scanf("%lld", &a) == 1;}
+bool RD(double & a) {return scanf("%lf", &a) == 1;}
+bool RD(char & a) {return scanf(" %c", &a) == 1;}
+bool RD(char * a) {return scanf("%s", a) == 1;}
+template<typename T, typename ... TT>
+bool RD(T & a, TT & ...  b) {return RD(a) && RD(b...);}
+
+/* Do princesses dream of magic sheep? */
+#define DRI(a) int a; RD(a)
+#define DRII(a,b) DRI(a); DRI(b)
+#define DRIII(a,b,c) DRI(a); DRII(b,c)
+#define DRIIII(a,b,c,d) DRI(a); DRIII(b,c,d)
+
+/* For it's time for you to fulfill your output. */
+void PT(const int a) {printf("%d", a);}
+void PT(const ll a) {printf("%lld", a);}
+void PT(const double a) {printf("%.16f", a);}
+void PT(const char a) {putchar(a);}
+void PT(const char * a) {fputs(a, stdout);}
+
+/* The line will last forever! */
+void PL(void) {PT('\n');}
+template<typename T, typename ... TT>
+void PL(const T a, const TT ...  b) {PT(a); if (sizeof...(b)) PT(' '); PL(b...);}
+
+/* Good Luck && Have Fun ! */
+const int N = 6e4+87;
+const double eps = 1e-6;
+double x[N],v[N];
+int main()
+{
+	DRI(n);
+	double lo = 0, hi=0;
+	RP(i,n) RD(x[i]);
+	RP(i,n) {
+		RD(v[i]);
+		cmax(hi, x[i]/v[i]);
+	}
+	while (fabs(hi - lo) > eps) {
+		double t = (hi+lo)/2;
+		double l = x[0] - v[0] * t;
+		double r = x[0] + v[0] * t;
+		REP(i,1,n) {
+			cmax(l, x[i] - v[i] * t);
+			cmin(r, x[i] + v[i] * t);
+		}
+		if (l <= r)
+			hi = t;
+		else
+			lo = t;
+	}
+	PL(hi);
+}
