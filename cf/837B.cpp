dc4457809980a8f2a77d03db8bea17b7b8696ba2
prpr <3254tg89@gmail.com>
2017-08-29 20:53:35 +0800

    cf 837B: implementation

---
+++ cf/837B.cpp
@@ -0,0 +1,138 @@
+#include <bits/stdc++.h>
+using namespace std;
+// nichijou
+#define REP(i,a,b) for (int i(a), _B(b); i < _B; ++i)
+#define RP(i,n) REP(i,0,n)
+#define PER(i,a,b) for(int i((a)-1), _B(b); i >= _B; --i)
+#define PR(i,n) PER(i,n,0)
+#define REP1(i,a,b) REP(i,a,(b)+1)
+#define RP1(i,n) REP1(i,1,n)
+#define PER1(i,a,b) PER(i,(a)+1,b)
+#define PR1(i,n) PER1(i,n,1)
+#define DO(n) REP(__i,0,n)
+template<typename T>
+void cmax(T & a, T b) {a = max(a, b);}
+template<typename T>
+void cmin(T & a, T b) {a = min(a, b);}
+
+// data type
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define F first
+#define S second
+
+// STL container
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define SZ(a) ((int)(a).size())
+#define ALL(a) begin(a), end(a)
+#define CLR(a) (a).clear()
+#define BK(a) ((a).back())
+#define FT(a) ((a).front())
+#define DB(a) (a).pop_back()
+#define DF(a) (a).pop_front()
+#define PB push_back
+#define EB emplace_back
+
+/* I gave you my heart and then you turned around. */
+void _BG(const char * s) {}
+template<typename T, typename ... TT>
+void _BG(const char * s,T a, TT...b)
+{
+	for (int c = 0; *s && (c || *s != ','); ++s) {
+		cerr<<*s;
+		for (char x : "([{") c += *s == x;
+		for (char x : ")]}") c -= *s == x;
+	}
+	cerr<<" = "<<a;
+	if (*s) {
+		cerr<<", ";
+		_BG(++s,b...);
+	} else
+		cerr<<endl;
+}
+#define BG(...) do { \
+	cerr << __PRETTY_FUNCTION__ << ':' << __LINE__ << ": "; \
+	_BG(#__VA_ARGS__,__VA_ARGS__); \
+} while(0)
+
+/* Reading input is now 20% cooler! */
+bool RD(void) {return true;}
+bool RD(char & a) {return scanf(" %c", &a) == 1;}
+bool RD(char * a) {return scanf("%s", a) == 1;}
+bool RD(double & a) {return scanf("%lf", &a) == 1;}
+bool RD(int & a) {return scanf("%d", &a) == 1;}
+bool RD(ll & a) {return scanf("%lld", &a) == 1;}
+
+template<typename T, typename ... TT>
+bool RD(T & a, TT & ...  b) {return RD(a) && RD(b...);}
+
+/* Do princesses dream of magic sheep? */
+#define DR(T,...) T __VA_ARGS__; RD(__VA_ARGS__)
+#define RI(...) DR(int,__VA_ARGS__)
+
+/* For it's time for you to fulfill your output. */
+void PT(const char a) {putchar(a);}
+void PT(const char * a) {fputs(a, stdout);}
+void PT(char * a) {fputs(a, stdout);}
+void PT(const double a) {printf("%.16f", a);}
+void PT(const int a) {printf("%d", a);}
+void PT(const ll a) {printf("%lld", a);}
+
+/* The line will last forever! */
+template<char sep = ' ',char end = '\n'>
+void PL(void) {if (end) PT(end);}
+template<char sep = ' ',char end = '\n',typename T, typename ... TT>
+void PL(const T a, const TT ...  b) {PT(a); if (sizeof...(b) && sep) PT(sep); PL<sep,end>(b...);}
+
+/* Good Luck && Have Fun ! */
+const int N = 106;
+const char * col = "RGB";
+char g[N][N];
+int id(char c)
+{
+	for (int i = 0; col[i]; ++i)
+		if (col[i] == c)
+			return i;
+	assert(2 + 2 == 5);
+}
+int eq(int l, int r,int u,int d)
+{
+	char x = g[l][u];
+	REP(i,l,r)
+		REP(j,u,d)
+			if (g[i][j] != x)
+				return -1;
+	return id(x);
+}
+int main()
+{
+	RI(n,m);
+	RP(i,n) RD(g[i]);
+	if (n%3 == 0) {
+		vi vis(3,0);
+		bool ok = 1;
+		RP(i,3) {
+			int x = eq(n/3*i,n/3*(i+1),0,m);
+			ok = ok && x >= 0 && vis[x]++ == 0;
+		}
+		if (ok) {
+			PL("YES");
+			return 0;
+		}
+	}
+	if (m%3 == 0) {
+		vi vis(3,0);
+		bool ok = 1;
+		RP(i,3) {
+			int x = eq(0,n,m/3*i,m/3*(i+1));
+			ok = ok && x >= 0 && vis[x]++ == 0;
+		}
+		if (ok) {
+			PL("YES");
+			return 0;
+		}
+	}
+	PL("NO");
+}
