d4396e403f2f641b34ee5925d74a995d41f65c6b
prpr <3254tg89@gmail.com>
2017-09-08 18:14:47 +0800

    cf 850C: combinatorial game, Sprague–Grundy, bitmask dp

---
+++ cf/850C.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+using namespace std;
+// nichijou
+#define REP(i,a,b) for (int i(a); i < (b); ++i)
+#define RP(i,n) REP(i,0,n)
+#define PER(i,a,b) for(int i((a)-1); i >= (b); --i)
+#define PR(i,n) PER(i,n,0)
+#define REP1(i,a,b) REP(i,a,(b)+1)
+#define RP1(i,n) REP1(i,1,n)
+#define PER1(i,a,b) PER(i,(a)+1,b)
+#define PR1(i,n) PER1(i,n,1)
+#define DO(n) RP(__i,n)
+template<class T>
+void cmax(T & a, T b) {a = max(a, b);}
+template<class T>
+void cmin(T & a, T b) {a = min(a, b);}
+
+// data type
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define F first
+#define S second
+
+// STL container
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define SZ(a) ((int)(a).size())
+#define ALL(a) begin(a), end(a)
+#define CLR(a) (a).clear()
+#define BK(a) ((a).back())
+#define FT(a) ((a).front())
+#define DB(a) (a).pop_back()
+#define DF(a) (a).pop_front()
+#define PB push_back
+#define EB emplace_back
+
+/* I gave you my heart and then you turned around. */
+void _BG(const char * s) {}
+template<class T, class ... TT>
+void _BG(const char * s,T a, TT...b)
+{
+	for (int c = 0; *s && (c || *s != ','); ++s) {
+		cerr<<*s;
+		for (char x : "([{") c += *s == x;
+		for (char x : ")]}") c -= *s == x;
+	}
+	cerr<<" = "<<a;
+	if (*s) {
+		cerr<<", ";
+		_BG(++s,b...);
+	} else
+		cerr<<endl;
+}
+#define BG(...) do { \
+	cerr << __PRETTY_FUNCTION__ << ':' << __LINE__ << ": "; \
+	_BG(#__VA_ARGS__,__VA_ARGS__); \
+} while(0)
+
+/* Reading input is now 20% cooler! */
+bool RD() {return 1;}
+bool RD(char & a) {return scanf(" %c", &a) == 1;}
+bool RD(char * a) {return scanf("%s", a) == 1;}
+bool RD(double & a) {return scanf("%lf", &a) == 1;}
+bool RD(int & a) {return scanf("%d", &a) == 1;}
+bool RD(ll & a) {return scanf("%lld", &a) == 1;}
+
+template<class T, class ... TT>
+bool RD(T & a, TT & ... b) {return RD(a) && RD(b...);}
+
+/* Do princesses dream of magic sheep? */
+#define DR(T,...) T __VA_ARGS__; RD(__VA_ARGS__)
+#define RI(...) DR(int,__VA_ARGS__)
+
+/* For it's time for you to fulfill your output. */
+void PT(const char & a) {putchar(a);}
+void PT(char const * const & a) {fputs(a, stdout);}
+void PT(const double & a) {printf("%.16f", a);}
+void PT(const int & a) {printf("%d", a);}
+void PT(const ll & a) {printf("%lld", a);}
+
+/* The line will last forever! */
+template<char s = ' ', char e = '\n'>
+void PL() {if (e) PT(e);}
+template<char s = ' ', char e = '\n', class T, class ... TT>
+void PL(const T & a, const TT & ... b)
+{PT(a); if (sizeof...(b) && s) PT(s); PL<s,e>(b...);}
+
+/* Good Luck && Have Fun ! */
+map<int,int> m;
+int sg(int s)
+{
+	if (__builtin_popcount(s) == 1)
+		return __lg(s) + 1;
+	auto it = m.lower_bound(s);
+	if (it != m.end() && it->F == s)
+		return it->S;
+	unsigned t = 0;
+	for (int i = __lg(s); i >= 0; --i)
+		t |= 1 << sg((s & ((1 << i) - 1)) | (s >> i >> 1));
+	t = ~t;
+	int i = __lg(t & ((~t)+1));
+	m.insert(it, {s,i});
+	return i;
+}
+int main()
+{
+	map<int,int> e;
+	RI(n);
+	int ans = 0;
+	RP(i,n) {
+		RI(a);
+		for (int j = 2; j * j <= a; ++j) {
+			int c = 0;
+			for (;a % j == 0; a/=j, ++c);
+			if (c)
+				e[j] |= 1<<(c-1);
+		}
+		if (a > 1)
+			e[a] |= 1;
+	}
+	for (const auto & p : e)
+		ans ^= sg(p.S);
+	PL(ans ? "Mojtaba" : "Arpa");
+}
