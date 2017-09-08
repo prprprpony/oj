ee9fdcef8a5382b903fd8e9503ce2671255d72f1
prpr <3254tg89@gmail.com>
2017-09-08 17:59:22 +0800

    cf 845: Educational Round 27:solved 4/7,ranked 137

---
+++ cf/845/B.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+// nichijou
+#define REP(i,a,b) for (int i(a); i < (b); ++i)
+#define RP(i,n) REP(i,0,n)
+#define PER(i,a,b) for(int i((a)-1); i >= (b); --i)
+#define PR(i,n) PER(i,n,0)
+#define REP1(i,a,b) REP(i,a,(b)+1)
+#define RP1(i,n) REP1(i,1,n)
+#define PER1(i,a,b) PER(i,(a)+1,b)
+#define PR1(i,n) PER1(i,n,1)
+template<class T>
+void cmax(T & a, T b) {a = max(a, b);}
+template<class T>
+void cmin(T & a, T b) {a = min(a, b);}
+
+// data type
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define F first
+#define S second
+
+// STL container
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define SZ(a) ((int)(a).size())
+#define ALL(a) begin(a), end(a)
+#define CLR(a) (a).clear()
+#define BK(a) ((a).back())
+#define FT(a) ((a).front())
+#define DB(a) (a).pop_back()
+#define DF(a) (a).pop_front()
+#define PB push_back
+#define EB emplace_back
+
+/* I gave you my heart and then you turned around. */
+void _BG(const char * s) {}
+template<class T, class ... TT>
+void _BG(const char * s,T a, TT...b)
+{
+	for (int c = 0; *s && (c || *s != ','); ++s) {
+		cerr<<*s;
+		for (char x : "([{") c += *s == x;
+		for (char x : ")]}") c -= *s == x;
+	}
+	cerr<<" = "<<a;
+	if (*s) {
+		cerr<<", ";
+		_BG(++s,b...);
+	} else
+		cerr<<endl;
+}
+#define BG(...) do { \
+	cerr << __PRETTY_FUNCTION__ << ':' << __LINE__ << ": "; \
+	_BG(#__VA_ARGS__,__VA_ARGS__); \
+} while(0)
+
+/* Reading input is now 20% cooler! */
+bool RD() {return 1;}
+bool RD(char & a) {return scanf(" %c", &a) == 1;}
+bool RD(char * a) {return scanf("%s", a) == 1;}
+bool RD(double & a) {return scanf("%lf", &a) == 1;}
+bool RD(int & a) {return scanf("%d", &a) == 1;}
+bool RD(ll & a) {return scanf("%lld", &a) == 1;}
+
+template<class T, class ... TT>
+bool RD(T & a, TT & ... b) {return RD(a) && RD(b...);}
+
+/* Do princesses dream of magic sheep? */
+#define DR(T,...) T __VA_ARGS__; RD(__VA_ARGS__)
+#define RI(...) DR(int,__VA_ARGS__)
+
+/* For it's time for you to fulfill your output. */
+void PT(const char a) {putchar(a);}
+void PT(const char * a) {fputs(a, stdout);}
+void PT(char * a) {fputs(a, stdout);}
+void PT(const double a) {printf("%.16f", a);}
+void PT(const int a) {printf("%d", a);}
+void PT(const ll a) {printf("%lld", a);}
+
+/* The line will last forever! */
+template<char s = ' ', char e = '\n'>
+void PL() {if (e) PT(e);}
+template<char s = ' ', char e = '\n', class T, class ... TT>
+void PL(const T&a,const TT&...b){PT(a);if(sizeof...(b)&&s)PT(s);PL<s,e>(b...);}
+
+/* Good Luck && Have Fun ! */
+int main()
+{
+	vi a,b;
+	int sa, sb;
+	sa = sb = 0;
+	int c;
+	RP(i,6) {
+		while (!isdigit(c = getchar()));
+		c -= '0';
+		(i < 3 ? a : b).PB(c);
+		(i < 3 ? sa += c : sb += c);
+	}
+	int ans = 0;
+	if (sa > sb) {
+		a.swap(b);
+		swap(sa,sb);
+	}
+	for (;sa < sb;++ans) {
+		sort(ALL(a));
+		sort(ALL(b));
+		int k = sb - sa;
+		if (9 - a[0] >  b[2]) {
+			k = min(k, 9 - a[0]);
+			a[0] += k;
+			sa += k;
+		} else {
+			k = min(k, b[2]);
+			b[2] -= k;
+			sb -= k;
+		}
+	}
+	PL(ans);
+}
