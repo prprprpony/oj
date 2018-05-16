2f5f8066615e3a9da8d4dd67a2c635f163d28d86
prpr <3254tg89@gmail.com>
2018-05-16 09:55:09 +0800

    cf 983: Codeforces Round #483 (Div.1): solved 2/5, ranked 143 B: math, dp C: dp

---
+++ cf/983/C.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+using namespace std;
+// nichijou
+#define REP(i,a,b) for (int i(a); i < (b); ++i)
+#define RP(i,n) REP(i,0,n)
+#define PER(i,a,b) for(int i((a)-1); i >= (b); --i)
+#define PR(i,n) PER(i,n,0)
+#define REP1(i,a,b) REP(i,a,(b)+1)
+#define RP1(i,n) REP1(i,1,n)
+#define PER1(i,a,b) PER(i,(a)+1,b)
+#define PR1(i,n) PER1(i,n,1)
+#define DO(n) RP(__i,n)
+template<class T,class U>
+bool cmax(T & a, const U & b) {return a < b ? a = b, 1 : 0;}
+template<class T,class U>
+bool cmin(T & a, const U & b) {return b < a ? a = b, 1 : 0;}
+
+// data type
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define F first
+#define S second
+
+// STL container
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define SZ(a) ((int)(a).size())
+#define ALL(a) begin(a), end(a)
+#define CLR(a) (a).clear()
+#define BK(a) ((a).back())
+#define FT(a) ((a).front())
+#define DB(a) (a).pop_back()
+#define DF(a) (a).pop_front()
+#define PB push_back
+#define EB emplace_back
+
+/* I gave you my heart and then you turned around. */
+void _BG(const char * s) {cerr<<s<<endl;};
+template<class T, class ... TT>
+void _BG(const char * s,T a, TT...b)
+{
+	for (int c = 0; *s && (c || *s != ','); ++s) {
+		cerr<<*s;
+		for (char x : "([{") c += *s == x;
+		for (char x : ")]}") c -= *s == x;
+	}
+	cerr<<" = "<<a;
+	if (sizeof...(b)) {
+		cerr<<", ";
+        ++s;
+    }
+    _BG(s,b...);
+}
+#define BG(...) do { \
+	cerr << __PRETTY_FUNCTION__ << ':' << __LINE__ << ": "; \
+	_BG(#__VA_ARGS__,__VA_ARGS__); \
+} while(0)
+
+/* Reading input is now 20% cooler! */
+bool RD() {return 1;}
+bool RD(char & a) {return scanf(" %c", &a) == 1;}
+bool RD(char * a) {return scanf("%s", a) == 1;}
+bool RD(double & a) {return scanf("%lf", &a) == 1;}
+bool RD(int & a) {return scanf("%d", &a) == 1;}
+bool RD(ll & a) {return scanf("%lld", &a) == 1;}
+
+template<class T, class ... TT>
+bool RD(T & a, TT & ... b) {return RD(a) && RD(b...);}
+
+/* Do princesses dream of magic sheep? */
+#define DR(T,...) T __VA_ARGS__; RD(__VA_ARGS__)
+#define RI(...) DR(int,__VA_ARGS__)
+
+/* For it's time for you to fulfill your output. */
+void PT(const char & a) {putchar(a);}
+void PT(char const * const & a) {fputs(a, stdout);}
+void PT(const double & a) {printf("%.16f", a);}
+void PT(const int & a) {printf("%d", a);}
+void PT(const ll & a) {printf("%lld", a);}
+
+/* The line will last forever! */
+template<char s = ' ', char e = '\n'>
+void PL() {if (e) PT(e);}
+template<char s = ' ', char e = '\n', class T, class ... TT>
+void PL(const T & a, const TT & ... b)
+{PT(a); if (sizeof...(b) && s) PT(s); PL<s,e>(b...);}
+
+/* Good Luck && Have Fun ! */
+map<int,int,greater<int>> dp[2];
+int dec(int s,int v[])
+{
+    int w = 0;
+    for (; s; ++w, s >>= 4)
+        v[w] = s & 15;
+    return w;
+}
+void upd(int i,int s,int v)
+{
+    auto it = dp[i].find(s);
+    if (it == end(dp[i])) {
+        dp[i][s] = v;
+    } else {
+        cmin(it->S, v);
+    }
+  //  BG(i,s,v,dp[i][s]);
+}
+int rmv(int s,int k)
+{
+    return  ((s >> ((k + 1) * 4)) << (k * 4)) | (s & ((1 << (k * 4)) - 1));
+}
+int main()
+{
+    RI(n);
+    dp[1][1] = 0;
+    int v[5];
+    RP(i,n) {
+        RI(a,b);
+        dp[i&1].clear();
+        for (auto it = begin(dp[i&1^1]); it != end(dp[i&1^1]); ++it) {
+            int s = it->F;
+            if (dec(s,v) == 5)
+                continue;
+            int j = s & 15;
+            s >>= 4;
+            upd(i&1, (s << 4 | b) << 4 | a , it->S + abs(j - a));
+        }
+        for (auto it = begin(dp[i&1]); it != end(dp[i&1]); ) {
+            int w = dec(it->F, v);   
+            int s = it->F;
+            int j = v[0];
+            REP(k,1,w) {
+                upd(i&1, (rmv(s, k) >> 4) << 4 | v[k] , it->S + abs(j - v[k]));
+            }
+            ++it;
+        }
+    }
+    int ans = 1e9;
+    RP1(i,9)
+        if (dp[n&1^1].count(i))
+            cmin(ans, dp[n&1^1][i]);
+    PL(ans + n * 2);
+}
