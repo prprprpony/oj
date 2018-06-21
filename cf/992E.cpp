769e0567822a7d045130bf9feebb92543d700f11
prpr <3254tg89@gmail.com>
2018-06-21 22:36:33 +0800

    cf 992E: Fenwick tree, binary search, O(Q log C log N)

---
+++ cf/992E.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+using namespace std;
+// nichijou
+#define REP(i,a,b) for (int i(a); i < (b); ++i)
+#define RP(i,n) REP(i,0,n)
+#define PER(i,a,b) for(int i((a)-1); i >= (b); --i)
+#define PR(i,n) PER(i,n,0)
+#define REP1(i,a,b) REP(i,a,(b)+1)
+#define RP1(i,n) REP1(i,1,n)
+#define PER1(i,a,b) PER(i,(a)+1,b)
+#define PR1(i,n) PER1(i,n,1)
+#define DO(n) RP(__i,n)
+template<class T,class U>
+bool cmax(T & a, const U & b) {return a < b ? a = b, 1 : 0;}
+template<class T,class U>
+bool cmin(T & a, const U & b) {return b < a ? a = b, 1 : 0;}
+
+// data type
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define F first
+#define S second
+
+// STL container
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define SZ(a) ((int)(a).size())
+#define ALL(a) begin(a), end(a)
+#define CLR(a) (a).clear()
+#define BK(a) ((a).back())
+#define FT(a) ((a).front())
+#define DB(a) (a).pop_back()
+#define DF(a) (a).pop_front()
+#define PB push_back
+#define EB emplace_back
+
+/* I gave you my heart and then you turned around. */
+void _BG(const char * s) {cerr<<s<<endl;};
+template<class T, class ... TT>
+void _BG(const char * s,T a, TT...b)
+{
+	for (int c = 0; *s && (c || *s != ','); ++s) {
+		cerr<<*s;
+		for (char x : "([{") c += *s == x;
+		for (char x : ")]}") c -= *s == x;
+	}
+	cerr<<" = "<<a;
+	if (sizeof...(b)) {
+		cerr<<", ";
+        ++s;
+    }
+    _BG(s,b...);
+}
+#define BG(...) do { \
+	cerr << __PRETTY_FUNCTION__ << ':' << __LINE__ << ": "; \
+	_BG(#__VA_ARGS__,__VA_ARGS__); \
+} while(0)
+
+/* Reading input is now 20% cooler! */
+bool RD() {return 1;}
+bool RD(char & a) {return scanf(" %c", &a) == 1;}
+bool RD(char * a) {return scanf("%s", a) == 1;}
+bool RD(double & a) {return scanf("%lf", &a) == 1;}
+bool RD(int & a) {return scanf("%d", &a) == 1;}
+bool RD(ll & a) {return scanf("%lld", &a) == 1;}
+
+template<class T, class ... TT>
+bool RD(T & a, TT & ... b) {return RD(a) && RD(b...);}
+
+/* Do princesses dream of magic sheep? */
+#define DR(T,...) T __VA_ARGS__; RD(__VA_ARGS__)
+#define RI(...) DR(int,__VA_ARGS__)
+
+/* For it's time for you to fulfill your output. */
+void PT(const char & a) {putchar(a);}
+void PT(char const * const & a) {fputs(a, stdout);}
+void PT(const double & a) {printf("%.16f", a);}
+void PT(const int & a) {printf("%d", a);}
+void PT(const ll & a) {printf("%lld", a);}
+
+/* The line will last forever! */
+template<char s = ' ', char e = '\n'>
+void PL() {if (e) PT(e);}
+template<char s = ' ', char e = '\n', class T, class ... TT>
+void PL(const T & a, const TT & ... b)
+{PT(a); if (sizeof...(b) && s) PT(s); PL<s,e>(b...);}
+
+/* Good Luck && Have Fun ! */
+const int N = 200721, C = 1e9;
+int a[N];
+ll fen[N];
+void add(int i,ll x)
+{
+    for (; i < N; i += i & -i)
+        fen[i] += x;
+}
+ll qry(int i)
+{
+    ll r = 0;
+    for (; i; i ^= i & -i)
+        r += fen[i];
+    return r;
+}
+int bs(ll x)
+{
+    int k = 0;
+    for (int i = 1<<__lg(N); i; i>>=1)
+        if ((k | i) < N && fen[k | i] < x)
+            x -= fen[k |= i];
+    return k + 1;
+}
+int main()
+{
+    RI(n,q);
+    RP1(i,n) {
+        RD(a[i]);
+        add(i,a[i]);
+    }
+    while (q--) {
+        RI(p,x);
+        add(p, x - a[p]);
+        a[p] = x;
+        if (a[1] == 0) {
+            PL(1);
+            continue;
+        }
+        int ans = -1;
+        ll v = a[1];
+        for (; v <= C; ) {
+            int k = bs(2 * v);
+            if (k > n)
+                break;
+            ll w = qry(k);
+            if (a[k] * 2 == w) {
+                ans = k;
+                break;
+            }
+            v = w;
+        }
+        PL(ans);
+    }
+}
