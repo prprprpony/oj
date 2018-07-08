ae56ab4255b8fbc1d6b42cd6a101e5415d07c859
prpr <3254tg89@gmail.com>
2018-07-08 16:30:29 +0800

    cf 894v: Round #447 (Div.2) virtual: solved 4/5, ranked 27 B: combinatorics D: centroid decomposition(?),  O((N + M) log^2 N) E: scc, dp

---
+++ cf/894v/B.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+using namespace std;
+// nichijou
+#define REP(i,a,b) for (int i(a); i < (b); ++i)
+#define RP(i,n) REP(i,0,n)
+#define PER(i,a,b) for(int i((a)-1); i >= (b); --i)
+#define PR(i,n) PER(i,n,0)
+#define REP1(i,a,b) REP(i,a,(b)+1)
+#define RP1(i,n) REP1(i,1,n)
+#define PER1(i,a,b) PER(i,(a)+1,b)
+#define PR1(i,n) PER1(i,n,1)
+#define DO(n) RP(__i,n)
+template<class T,class U>
+bool cmax(T & a, const U & b) {return a < b ? a = b, 1 : 0;}
+template<class T,class U>
+bool cmin(T & a, const U & b) {return b < a ? a = b, 1 : 0;}
+
+// data type
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define F first
+#define S second
+
+// STL container
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define SZ(a) ((int)(a).size())
+#define ALL(a) begin(a), end(a)
+#define CLR(a) (a).clear()
+#define BK(a) ((a).back())
+#define FT(a) ((a).front())
+#define DB(a) (a).pop_back()
+#define DF(a) (a).pop_front()
+#define PB push_back
+#define EB emplace_back
+
+/* I gave you my heart and then you turned around. */
+void _BG(const char * s) {cerr<<s<<endl;};
+template<class T, class ... TT>
+void _BG(const char * s,T a, TT...b)
+{
+	for (int c = 0; *s && (c || *s != ','); ++s) {
+		cerr<<*s;
+		for (char x : "([{") c += *s == x;
+		for (char x : ")]}") c -= *s == x;
+	}
+	cerr<<" = "<<a;
+	if (sizeof...(b)) {
+		cerr<<", ";
+        ++s;
+    }
+    _BG(s,b...);
+}
+#define BG(...) do { \
+	cerr << __PRETTY_FUNCTION__ << ':' << __LINE__ << ": "; \
+	_BG(#__VA_ARGS__,__VA_ARGS__); \
+} while(0)
+
+/* Reading input is now 20% cooler! */
+bool RD() {return 1;}
+bool RD(char & a) {return scanf(" %c", &a) == 1;}
+bool RD(char * a) {return scanf("%s", a) == 1;}
+bool RD(double & a) {return scanf("%lf", &a) == 1;}
+bool RD(int & a) {return scanf("%d", &a) == 1;}
+bool RD(ll & a) {return scanf("%lld", &a) == 1;}
+
+template<class T, class ... TT>
+bool RD(T & a, TT & ... b) {return RD(a) && RD(b...);}
+
+/* Do princesses dream of magic sheep? */
+#define DR(T,...) T __VA_ARGS__; RD(__VA_ARGS__)
+#define RI(...) DR(int,__VA_ARGS__)
+
+/* For it's time for you to fulfill your output. */
+void PT(const char & a) {putchar(a);}
+void PT(char const * const & a) {fputs(a, stdout);}
+void PT(const double & a) {printf("%.16f", a);}
+void PT(const int & a) {printf("%d", a);}
+void PT(const ll & a) {printf("%lld", a);}
+
+/* The line will last forever! */
+template<char s = ' ', char e = '\n'>
+void PL() {if (e) PT(e);}
+template<char s = ' ', char e = '\n', class T, class ... TT>
+void PL(const T & a, const TT & ... b)
+{PT(a); if (sizeof...(b) && s) PT(s); PL<s,e>(b...);}
+
+/* Good Luck && Have Fun ! */
+void bf()
+{
+    int a[10][10];
+    for (int k = -1; k <= 1; k += 2) {
+        RP1(n,5)
+            RP1(m,5) {
+                BG(n,m,k);
+                int ans = 0;
+                for (int s = 0; s < (1<<(n*m)); ++s) {
+                    int t = s;
+                    RP(i,n)
+                        RP(j,m) {
+                            a[i][j] = (t&1) ? 1 : -1;
+                            t>>=1;
+                        }
+                    bool ok = 1;
+                    RP(i,n) {
+                        int r = 1;
+                        RP(j,m) r *= a[i][j];
+                        ok &= r == k;
+                    }
+                    RP(j,m) {
+                        int r = 1;
+                        RP(i,n) r *= a[i][j];
+                        ok &= r == k;
+                    }
+                    ans += ok;
+                }
+                BG(ans);
+            }
+    }
+}
+ll qpow(ll a,ll b)
+{
+    const ll M = 1e9 + 7;
+    ll r = 1;
+    for (; b; b>>=1, a=a*a%M)
+        if (b & 1)
+            r = r * a % M;
+    return r;
+}
+int main()
+{
+    ll n,m,k;
+    RD(n,m,k);
+    if (k == -1 && (n + m) % 2) {
+        PL(0);
+        return 0;
+    }
+    PL(qpow(qpow(2,n-1),m-1));
+}
---
+++ cf/894v/D.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+using namespace std;
+// nichijou
+#define REP(i,a,b) for (int i(a); i < (b); ++i)
+#define RP(i,n) REP(i,0,n)
+#define PER(i,a,b) for(int i((a)-1); i >= (b); --i)
+#define PR(i,n) PER(i,n,0)
+#define REP1(i,a,b) REP(i,a,(b)+1)
+#define RP1(i,n) REP1(i,1,n)
+#define PER1(i,a,b) PER(i,(a)+1,b)
+#define PR1(i,n) PER1(i,n,1)
+#define DO(n) RP(__i,n)
+template<class T,class U>
+bool cmax(T & a, const U & b) {return a < b ? a = b, 1 : 0;}
+template<class T,class U>
+bool cmin(T & a, const U & b) {return b < a ? a = b, 1 : 0;}
+
+// data type
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define F first
+#define S second
+
+// STL container
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define SZ(a) ((int)(a).size())
+#define ALL(a) begin(a), end(a)
+#define CLR(a) (a).clear()
+#define BK(a) ((a).back())
+#define FT(a) ((a).front())
+#define DB(a) (a).pop_back()
+#define DF(a) (a).pop_front()
+#define PB push_back
+#define EB emplace_back
+
+/* I gave you my heart and then you turned around. */
+void _BG(const char * s) {cerr<<s<<endl;};
+template<class T, class ... TT>
+void _BG(const char * s,T a, TT...b)
+{
+	for (int c = 0; *s && (c || *s != ','); ++s) {
+		cerr<<*s;
+		for (char x : "([{") c += *s == x;
+		for (char x : ")]}") c -= *s == x;
+	}
+	cerr<<" = "<<a;
+	if (sizeof...(b)) {
+		cerr<<", ";
+        ++s;
+    }
+    _BG(s,b...);
+}
+#define BG(...) do { \
+	cerr << __PRETTY_FUNCTION__ << ':' << __LINE__ << ": "; \
+	_BG(#__VA_ARGS__,__VA_ARGS__); \
+} while(0)
+
+/* Reading input is now 20% cooler! */
+bool RD() {return 1;}
+bool RD(char & a) {return scanf(" %c", &a) == 1;}
+bool RD(char * a) {return scanf("%s", a) == 1;}
+bool RD(double & a) {return scanf("%lf", &a) == 1;}
+bool RD(int & a) {return scanf("%d", &a) == 1;}
+bool RD(ll & a) {return scanf("%lld", &a) == 1;}
+
+template<class T, class ... TT>
+bool RD(T & a, TT & ... b) {return RD(a) && RD(b...);}
+
+/* Do princesses dream of magic sheep? */
+#define DR(T,...) T __VA_ARGS__; RD(__VA_ARGS__)
+#define RI(...) DR(int,__VA_ARGS__)
+
+/* For it's time for you to fulfill your output. */
+void PT(const char & a) {putchar(a);}
+void PT(char const * const & a) {fputs(a, stdout);}
+void PT(const double & a) {printf("%.16f", a);}
+void PT(const int & a) {printf("%d", a);}
+void PT(const ll & a) {printf("%lld", a);}
+
+/* The line will last forever! */
+template<char s = ' ', char e = '\n'>
+void PL() {if (e) PT(e);}
+template<char s = ' ', char e = '\n', class T, class ... TT>
+void PL(const T & a, const TT & ... b)
+{PT(a); if (sizeof...(b) && s) PT(s); PL<s,e>(b...);}
+
+/* Good Luck && Have Fun ! */
+const int N = 1e6 + 87;
+int w[N];
+vector<ll> d[N], s[N];
+ll qs(int u,ll v)
+{
+    int k = lower_bound(ALL(d[u]), v) - begin(d[u]);
+    if (k == 0)
+        return 0;
+    return k * v - s[u][k-1];
+}
+ll qry(int u,ll h)
+{
+    ll ans = qs(u,h);
+    for (int v = u/2; v; u = v, v /= 2) {
+        ans += qs(v,h-w[u]) - qs(u,h-w[u]*2);
+        h -= w[u];
+    }
+    return ans;
+}
+int main()
+{
+    RI(n,m);
+    REP1(i,2,n)
+        RD(w[i]);
+    PER1(i,n,1) {
+        d[i].PB(0);
+        sort(ALL(d[i]));
+        s[i].resize(SZ(d[i]));
+        partial_sum(ALL(d[i]),begin(s[i]));
+        for (const auto & x : d[i])
+            d[i/2].PB(x + w[i]);
+    }
+    while (m--) {
+        RI(a,h);
+        PL(qry(a,h));
+    }
+}
