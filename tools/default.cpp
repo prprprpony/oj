e33d8200034f91f6f9a9f36074ad11bf184368fa
prpr <3254tg89@gmail.com>
2018-12-12 12:01:35 +0800

    default.cpp: add pb_ds: gp_hash_table, mergeable heap, order statistics set

--- tools/default.cpp
+++ tools/default.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
+#include <bits/extc++.h>
 using namespace std;
-// nichijou
+/* nichijou */
 #define REP(i,a,b) for (int i(a); i < (b); ++i)
 #define RP(i,n) REP(i,0,n)
 #define PER(i,a,b) for(int i((a)-1); i >= (b); --i)
@@ -14,15 +15,13 @@ template<class T,class U>
 bool cmax(T & a, const U & b) {return a < b ? a = b, 1 : 0;}
 template<class T,class U>
 bool cmin(T & a, const U & b) {return b < a ? a = b, 1 : 0;}
-
-// data type
+/* data type */
 typedef long long ll;
 typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
 #define F first
 #define S second
-
-// STL container
+/* STL container */
 typedef vector<int> vi;
 typedef vector<ll> vll;
 #define SZ(a) ((int)(a).size())
@@ -34,7 +33,16 @@ typedef vector<ll> vll;
 #define DF(a) (a).pop_front()
 #define PB push_back
 #define EB emplace_back
-
+/* pb_ds */
+#define umap_t __gnu_pbds::gp_hash_table
+// umap_t<key_type,value_type,key_hasher_functor>
+#define heap_t __gnu_pbds::priority_queue
+// heap_t<value_type,compare_functor>
+// h1.join(h2); assert(h2.empty());
+template<class T>
+using set_t = __gnu_pbds::tree<T,__gnu_pbds::null_type,less<T>,__gnu_pbds::rb_tree_tag,__gnu_pbds::tree_order_statistics_node_update>;
+// s.order_of_key(k) number of element less than k
+// s.find_by_order(k) 0-based
 /* I gave you my heart and then you turned around. */
 void _BG(const char * s) {cerr<<s<<endl;}
 template<class T, class ... TT>
@@ -57,7 +65,6 @@ void _BG(const char * s,T a, TT...b)
 #else
 #define BG(...)
 #endif
-
 /* Reading input is now 20% cooler! */
 bool RD() {return 1;}
 bool RD(char & a) {return scanf(" %c", &a) == 1;}
@@ -65,28 +72,23 @@ bool RD(char * a) {return scanf("%s", a) == 1;}
 bool RD(double & a) {return scanf("%lf", &a) == 1;}
 bool RD(int & a) {return scanf("%d", &a) == 1;}
 bool RD(ll & a) {return scanf("%lld", &a) == 1;}
-
 template<class T, class ... TT>
 bool RD(T & a, TT & ... b) {return RD(a) && RD(b...);}
-
 /* Do princesses dream of magic sheep? */
 #define DR(T,...) T __VA_ARGS__; RD(__VA_ARGS__)
 #define RI(...) DR(int,__VA_ARGS__)
-
 /* For it's time for you to fulfill your output. */
 void PT(const char & a) {putchar(a);}
 void PT(char const * const & a) {fputs(a, stdout);}
 void PT(const double & a) {printf("%.16f", a);}
 void PT(const int & a) {printf("%d", a);}
 void PT(const ll & a) {printf("%lld", a);}
-
 /* The line will last forever! */
 template<char s = ' ', char e = '\n'>
 void PL() {if (e) PT(e);}
 template<char s = ' ', char e = '\n', class T, class ... TT>
 void PL(const T & a, const TT & ... b)
 {PT(a); if (sizeof...(b) && s) PT(s); PL<s,e>(b...);}
-
 /* Good Luck && Have Fun ! */
 int main()
 {
