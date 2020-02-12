6fd7889d99c660e99425040031326e6fb1dfcaa5
prpr <3254tg89@gmail.com>
2020-02-12 18:21:13 +0800

    cf 1220D: math, graph theory

---
+++ cf/1220D.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* nichijou */
+template<class T,class U>
+bool cmax(T & a, const U & b) {return a < b ? a = b, 1 : 0;}
+template<class T,class U>
+bool cmin(T & a, const U & b) {return b < a ? a = b, 1 : 0;}
+/* data type */
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define F first
+#define S second
+/* STL container */
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define SZ(a) ((int)(a).size())
+#define ALL(a) begin(a), end(a)
+#define CLR(a) (a).clear()
+#define BK(a) ((a).back())
+#define FT(a) ((a).front())
+#define DB(a) (a).pop_back()
+#define DF(a) (a).pop_front()
+#define PB push_back
+#define EB emplace_back
+/* I gave you my heart and then you turned around. */
+void _BG(const char * s) {cerr<<s<<endl;}
+template<class T, class ... TT>
+void _BG(const char * s,T a, TT...b)
+{
+    for (size_t c = 0; *s && (c || *s != ','); cerr << *s++)
+        c += count(ALL("([{"),*s) - count(ALL(")]}"),*s);
+    cerr << " = " << a;
+    if (*s) {
+        cerr << ", ";
+        ++s;
+    }
+    _BG(s,b...);
+}
+#ifdef PR3PONY
+#define BG(...) do { \
+    cerr << __PRETTY_FUNCTION__ << ':' << __LINE__ << ": "; \
+    _BG(#__VA_ARGS__,__VA_ARGS__); \
+} while(0)
+#else
+#define BG(...)
+#endif
+/* Good Luck && Have Fun ! */
+const int N = 2e5  + 87;
+ll a[N];
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    map<int,int> cnt;
+    for (int i = 0; i < n; ++i) {
+        ll x;
+        cin >> x;
+        cnt[__lg(x & -x)]++;
+        a[i] = x;
+    } 
+    pii ans = pii(n, -1);
+    for (auto p : cnt)
+        ans = min(ans, pii(n - p.S, p.F));
+    cout << ans.F << endl;
+    for (int i = 0; i < n; ++i)
+        if (__lg(a[i] & -a[i]) != ans.S)
+            cout << a[i] << " \n"[--ans.F == 0];
+}
